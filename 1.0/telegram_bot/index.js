586e2b80e59317d4948ab935726df3ff2f0b7774
Nakonechnyi-Maksim <nakonechnyi1234567890@gmail.com>
2023-09-06 18:44:18 +0400

    #

--- 1.0/telegram_bot/index.js
+++ 1.0/telegram_bot/index.js
@@ -11,10 +11,10 @@ const DB = {};
 bot.setMyCommands([
   { command: "/start", description: "Приветствие" },
   { command: "/info", description: "Информация что может этот бот" },
-  { command: "/ebal", description: "Проверка" },
+  { command: "/labe", description: "Проверка" },
 ]);
 
-const ebalMamy = async (chatId) => {
+const func = async (chatId) => {
   await bot.sendMessage(chatId, "А теперь проверим", button);
 };
 
@@ -23,7 +23,7 @@ const start = () => {
     const text = msg.text;
     const chatId = msg.chat.id;
     if (text === "/start") {
-      await bot.sendMessage(chatId, `Привет, ${msg.from.first_name} ты пидор`);
+      await bot.sendMessage(chatId, `Привет, ${msg.from.first_name}`);
       await bot.sendSticker(
         chatId,
         `https://tlgrm.ru/_/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/7.webp`
@@ -32,8 +32,8 @@ const start = () => {
     if (text === "/info") {
       await bot.sendMessage(chatId, "Этот бот считает количество ");
     }
-    if (text === "/ebal") {
-      return ebalMamy(chatId);
+    if (text === "/labe") {
+      return func(chatId);
     }
   });
 
@@ -44,7 +44,7 @@ const start = () => {
     await bot.sendMessage(chatId, `Ты сделал свой выбор`, againButton);
     DB[chatId] = msg.data;
     if (data === "/again") {
-      return ebalMamy(chatId);
+      return func(chatId);
     }
     console.log(msg);
     console.log(DB);
