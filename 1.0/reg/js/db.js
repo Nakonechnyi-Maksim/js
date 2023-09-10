faca3279fcb6c42566736e54b6df2f6043e6adb0
Nakonechnyi-Maksim <nakonechnyi1234567890@gmail.com>
2023-09-11 01:30:47 +0400

    #

--- 1.0/reg/js/db.js
+++ 1.0/reg/js/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize("", "postgres", "root", {
+module.exports = new Sequelize("node_reg_user", "postgres", "root", {
   host: "localhost",
   port: "5432",
   dialect: "postgres",
--- 1.0/reg/js/src.js
+++ 1.0/reg/js/src.js
@@ -2,7 +2,14 @@ const button = document.getElementById("btn");
 const userEmail = document.getElementById("email");
 const userPass = document.getElementById("password");
 
-button.addEventListener("click", () => {
+const sequelize = require("./db");
+
+const UserModel = require("./model");
+
+button.addEventListener("click", async () => {
+  await sequelize.authenticate();
+  await sequelize.sync();
+
   if (userEmail.value == "") {
     alert("Email пустой");
   }
