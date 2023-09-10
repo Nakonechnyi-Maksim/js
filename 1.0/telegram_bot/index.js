bba639bf073830aeca7f053c70833dfbdb6f0c16
Nakonechnyi-Maksim <nakonechnyi1234567890@gmail.com>
2023-09-10 05:22:00 +0400

    1

--- 1.0/telegram_bot/index.js
+++ 1.0/telegram_bot/index.js
@@ -4,11 +4,11 @@ const { button, againButton } = require("./options.js");
 
 const { token } = require("./token.js");
 
-const bot = new TelegramApi(token, { polling: true });
-
 const sequelize = require("./db");
 
 const UserModel = require("./models");
+
+const bot = new TelegramApi(token, { polling: true });
 
 bot.setMyCommands([
   { command: "/start", description: "Приветствие" },
@@ -22,48 +22,40 @@ const func = async (chatId) => {
 };
 
 const start = async () => {
-  try {
-    await sequelize.authenticate();
-    await sequelize.sync();
-  } catch (error) {
-    console.log("Ошибка емае");
-  }
+  await sequelize.authenticate();
+  await sequelize.sync();
 
   bot.on("message", async (msg) => {
     const text = msg.text;
     const chatId = msg.chat.id;
 
-    try {
-      if (text === "/start") {
-        await UserModel.create({ chatId });
-        await bot.sendMessage(chatId, `Привет, ${msg.from.first_name}`);
-        await bot.sendSticker(
-          chatId,
-          `https://tlgrm.ru/_/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/7.webp`
-        );
-      }
-      if (text === "/info") {
-        await bot.sendMessage(chatId, "Этот бот считает количество лайков");
-      }
-      if (text === "/like") {
-        return func(chatId);
-      }
-      if (text === "/amount") {
-        await bot.sendMessage(chatId, `Количество Да ${User.amount_Yes}`);
-      }
-      bot.on("callback_query", async (msg) => {
-        const data = msg.data;
-        const chatId = msg.message.chat.id;
-
-        if (data === "/again") {
-          return func(chatId);
-        }
-
-        await bot.sendMessage(chatId, `Ты сделал свой выбор`, againButton);
-      });
-    } catch (error) {
-      return bot.sendMessage(chatId, "Ошибка емае");
+    if (text === "/start") {
+      await UserModel.create({ chatId });
+      await bot.sendMessage(chatId, `Привет, ${msg.from.first_name}`);
+      await bot.sendSticker(
+        chatId,
+        `https://tlgrm.ru/_/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/7.webp`
+      );
     }
+    if (text === "/info") {
+      await bot.sendMessage(chatId, "Этот бот считает количество лайков");
+    }
+    if (text === "/like") {
+      await func(chatId);
+    }
+    if (text === "/amount") {
+      const user = await UserModel.findOne({ chatId });
+      return bot.sendMessage(chatId, `Количество Да ${user.amount_Yes}`);
+    }
+  });
+  bot.on("callback_query", async (msg) => {
+    const data = msg.data;
+    const chatId = msg.message.chat.id;
+
+    if (data === "/again") {
+      return func(chatId);
+    }
+    await bot.sendMessage(chatId, `Ты сделал свой выбор`, againButton);
   });
 };
 
