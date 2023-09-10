25ccf98282f619352f5879b0f7e95f05429f05fd
Nakonechnyi-Maksim <nakonechnyi1234567890@gmail.com>
2023-09-10 13:21:46 +0400

    1

--- 1.0/telegram_bot/index.js
+++ 1.0/telegram_bot/index.js
@@ -28,45 +28,49 @@ const start = async () => {
   bot.on("message", async (msg) => {
     const text = msg.text;
     const chatId = msg.chat.id;
-
-    if (text === "/start") {
-      await UserModel.create({ chatId });
-      await bot.sendMessage(chatId, `Привет, ${msg.from.first_name}`);
-      await bot.sendSticker(
-        chatId,
-        `https://tlgrm.ru/_/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/7.webp`
-      );
-    }
-    if (text === "/info") {
-      await bot.sendMessage(chatId, "Этот бот считает количество лайков");
-    }
-    if (text === "/like") {
-      await func(chatId);
-    }
-    if (text === "/amount") {
-      const user = await UserModel.findOne({ chatId });
-      await bot.sendMessage(
-        chatId,
-        `Количество Да ${user.amount_Yes} Нет ${user.amount_No}`
-      );
-      await user.save();
+    try {
+      if (text === "/start") {
+        await UserModel.create({ chatId });
+        await bot.sendMessage(chatId, `Привет, ${msg.from.first_name}`);
+        await bot.sendSticker(
+          chatId,
+          `https://tlgrm.ru/_/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/7.webp`
+        );
+      }
+      if (text === "/info") {
+        await bot.sendMessage(chatId, "Этот бот считает количество лайков");
+      }
+      if (text === "/like") {
+        await func(chatId);
+      }
+      if (text === "/amount") {
+        const user = await UserModel.findOne({ chatId });
+        await bot.sendMessage(
+          chatId,
+          `Количество Да ${user.amount_Yes} Нет ${user.amount_No}`
+        );
+      }
+    } catch (error) {
+      return bot.sendMessage(chatId, "Ашибочка");
     }
   });
   bot.on("callback_query", async (msg) => {
     const data = msg.data;
     const chatId = msg.message.chat.id;
-    const user = await UserModel.findOne({ chatId });
+
     if (data === "/again") {
-      return func(chatId);
+      await func(chatId);
     }
+
+    const user = await UserModel.findOne({ chatId });
     if (data == "Да") {
       user.amount_Yes += 1;
     }
     if (data == "Нет") {
       user.amount_No += 1;
     }
-    await bot.sendMessage(chatId, `Ты сделал свой выбор`, againButton);
     await user.save();
+    await bot.sendMessage(chatId, `Ты сделал свой выбор`, againButton);
   });
 };
 
